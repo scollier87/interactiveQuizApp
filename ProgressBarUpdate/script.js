500b69e58db7d0d540af73355138dc942b1947a7
Xaclew <130411117+Xaclew@users.noreply.github.com>
2023-11-15 19:35:59 -0500

    Add files via upload

---
+++ ProgressBarUpdate/script.js
@@ -0,0 +1,40 @@
+// JavaScript object to store quiz questions and answers
+
+var quizQuestions = [
+    {
+        question: "What is the capital of France?",
+        options: ["Berlin", "Madrid", "Paris", "Rome"],
+        correctAnswer: "Paris"
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        options: ["Venus", "Mars", "Jupiter", "Saturn"],
+        correctAnswer: "Mars"
+    },
+    {
+        question: "The Eiffel Tower is located in which city?",
+        options: ["London", "Paris", "New York", "Tokyo"],
+        correctAnswer: "Paris"
+    },
+    {
+        question: "What is the capital of France?",
+        options: ["Berlin", "Madrid", "Paris", "Rome"],
+        correctAnswer: "Paris"
+    },
+    {
+        question: "JavaScript is a programming Language commonly used for both front-end and back-end web development.",
+        options: ["True", "False"],
+        correctAnswer: "True"
+    },
+    {
+        question: "The currency of Japan is Yuan",
+        options: ["True", "False"],
+        correctAnswer: "False"
+    },
+];
+
+//Testing to see if the data is accessible
+
+console.log("Question 1:", quizQuestions[0].question);
+console.log("Options for Question 1:", quizQuestions[0].options);
+console.log("Correct Answer for Question 1:",quizQuestions[0].correctAnswer);
